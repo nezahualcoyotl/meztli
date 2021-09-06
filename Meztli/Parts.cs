d1e7b644b285b12dd6e00ba22e9fbff6d24a8013
d <damian@nemesys.io>
2021-09-05 20:06:08 -0500

    adding parts form

---
+++ Meztli/Parts.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Meztli
+{
+    public partial class Parts : Form
+    {
+        public Parts()
+        {
+            InitializeComponent();
+        }
+    }
+}
